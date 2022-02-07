9b8a9999e5cad8f2ad4b4bc0e583e5a051d0b3f2
Caique Noboa <caique.noboa@mercadolivre.com>
2022-02-07 15:01:53 -0300

    JSON Encoding

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 
 	//define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	//starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -18,4 +26,13 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"Caique", "New Delhi", "110076"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
